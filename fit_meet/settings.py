2cec2ba8cddb03696633fca97bdf6b6578f6c3ad
Ofekmn <ofekm3000@gmail.com>
2023-04-10 22:39:04 +0300

    Add Category app

--- fit_meet/settings.py
+++ fit_meet/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'static_home_page.apps.StaticHomePageConfig',
     'location.apps.LocationConfig',
+    'category.apps.CategoryConfig',
 ]
 
 MIDDLEWARE = [
