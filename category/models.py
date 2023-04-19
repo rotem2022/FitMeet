0432bcc35042e4184452d92aa58d9701c78e53f3
Ofekmn <ofekm3000@gmail.com>
2023-04-19 22:55:37 +0300

    Update category model
    adds the ability the update a category,
    and a verification for if the category already exists.
    Also adds a verification for a valid category name.
    
    Signed-off-by: Ofekmn <ofekm3000@gmail.com>

--- category/models.py
+++ category/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+from django.core.validators import RegexValidator
+
+
+valid_name = RegexValidator(r'^[&0-9a-zA-Z\s_\.\,-]+$',
+                            'Only alphanumeric characters, and ,.-_& are allowed.')
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True, validators=[valid_name])
 
     def __str__(self):
         return self.name
+
+    def update(self, name):
+        self.name = name
+        self.full_clean()
+        self.save()
--- category/tests.py
+++ category/tests.py
@@ -1,3 +1,59 @@
-# from django.test import TestCase
+import string
+import re
+import pytest
+from django.db import IntegrityError
+from django.core.exceptions import ValidationError
+from .models import Category
 
-# Create your tests here.
+CATEGORY_NAME = "category1"
+CATEGORY_NAME_NEW = "category2"
+LONG_NAME = "a" * 25
+EMPTY_NAME = ''
+SPECIAL_CHARACTERS = re.sub("[&_.,-]", "", string.punctuation)
+
+
+@pytest.fixture
+def category1():
+    category1 = Category.objects.create(name=CATEGORY_NAME)
+    category1.save()
+    return category1
+
+
+@pytest.mark.django_db
+class TestCategoryModel:
+
+    def test_update_category(self, category1):
+        assert category1.name == CATEGORY_NAME
+        category1.update(CATEGORY_NAME_NEW)
+        updated_category = Category.objects.get(id=category1.id)
+        assert updated_category.name == CATEGORY_NAME_NEW
+
+    @pytest.mark.parametrize(
+        "invalid_name",
+        [EMPTY_NAME, LONG_NAME, SPECIAL_CHARACTERS],
+    )
+    def test_update_category_with_invalid_name(self, category1, invalid_name):
+        assert category1.name == CATEGORY_NAME
+        with pytest.raises(ValidationError):
+            category1.update(invalid_name)
+
+    def test_category_with_same_name(self, category1):
+        with pytest.raises(IntegrityError):
+            Category.objects.create(name=CATEGORY_NAME)
+
+    @pytest.mark.parametrize(
+        "invalid_name",
+        [EMPTY_NAME, LONG_NAME, SPECIAL_CHARACTERS],
+    )
+    def test_create_category_with_invalid_name(self, invalid_name):
+        with pytest.raises(ValidationError):
+            Category.objects.create(name=invalid_name).full_clean()
+
+    @pytest.mark.parametrize(
+        "valid_name",
+        ['b1', 'B', 'a space', '_', '-', ',', '.', '&'],
+    )
+    def test_create_category_with_valid_name(self, valid_name):
+        category = Category.objects.create(name=valid_name)
+        category.full_clean()
+        assert category.name == valid_name
