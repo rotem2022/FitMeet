7311feed4cebe2f62d4643fb2e9aeb78ba4593a9
Daniel Senderovych <danielsen@mta.ac.il>
2023-04-19 11:44:09 +0300

    Adding a event field to the Poll model
    which will be used to store the
    event id of the event that the poll is related to.
    Updating migrations files
    
    Co-authored-by: Ofek Manor <ofekm3000@gmail.com>
    Signed-off-by: Daniel Senderovych <danielsen@mta.ac.il>

--- poll/models.py
+++ poll/models.py
@@ -7,5 +7,7 @@ def get_default_end_date():
 
 
 class Poll(models.Model):
+    event_id = models.OneToOneField(to='event.Event', on_delete=models.CASCADE,
+                                    related_name='poll_ref', null=True)
     max_suggestions = models.PositiveIntegerField()
     end_time = models.DateTimeField(default=get_default_end_date)
