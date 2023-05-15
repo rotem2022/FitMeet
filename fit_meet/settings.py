548f6869dabd236c557662c47ba853d64734e0ad
Tal Koren <talko4245@gmail.com>
2023-05-15 11:36:34 +0300

    Extend event templates with base templates
    This extends the create event and event info templates with the base template
    Also modify the project setting file to know the path for each template directory
    And add blocks to event info template to allow injection of of the teams and poll components
    
    Signed-off-by: Tal Koren <talko4245@gmail.com>

--- fit_meet/settings.py
+++ fit_meet/settings.py
@@ -6,7 +6,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -60,7 +60,10 @@ ROOT_URLCONF = 'fit_meet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'static_home_page', 'templates'),
+            os.path.join(BASE_DIR, 'event', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
