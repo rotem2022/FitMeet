0ccfed22c99f2b996afa1669f84ffedf1608a593
Tal Koren <talko4245@gmail.com>
2023-05-17 16:33:02 +0300

    Create an admin profile and connect it to the admin superuser
    Signed-off-by: Tal Koren <talko4245@gmail.com>

--- users/migrations/create_superuser.py
+++ users/migrations/create_superuser.py
@@ -1,15 +1,19 @@
 from __future__ import unicode_literals
+from django.utils import timezone
 from django.db import migrations, transaction
 from django.contrib.auth.models import User
+from ..models import Profile
 
 
 def create_superuser(apps, schema_editor):
     with transaction.atomic():
-        superuser = User(
-            is_superuser=True, is_staff=True, username='admin', email='admin@gmail.com'
-        )
+        superuser = User(is_superuser=True, is_staff=True, username='admin', email='admin@gmail.com')
         superuser.set_password('adminPassword')
         superuser.save()
+        admin_profile = Profile(
+            user=superuser, date_of_birth=timezone.datetime(2020, 5, 17), phone_number='+972521234567'
+        )
+        admin_profile.save()
 
 
 class Migration(migrations.Migration):
