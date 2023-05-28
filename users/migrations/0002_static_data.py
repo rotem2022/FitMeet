4bd5bab89ca66d287ebc63e6efafbdc3290c688a
Ofekmn <ofekm3000@gmail.com>
2023-05-28 18:04:57 +0300

    add first_name, last_name to the users.
    Signed-off-by: Ofekmn <ofekm3000@gmail.com>

--- users/migrations/0002_static_data.py
+++ users/migrations/0002_static_data.py
@@ -8,17 +8,19 @@ def create_initial_users(apps, schema_editor):
     Profile = apps.get_model('users', 'Profile')
     User = apps.get_model('auth', 'User')
     profile_data = [
-        ('Ofek', 'ofekPassword', datetime(2020, 5, 17), '050-123456789'),
-        ('Danny', 'dannyPassword', datetime(2020, 5, 17), '050-223456789'),
-        ('Rimon', 'rimonPassword', datetime(2020, 5, 17), '050-323456789'),
-        ('Rotem', 'rotemPassword', datetime(2020, 5, 17), '050-423456789'),
-        ('Saar', 'saarPassword', datetime(2020, 5, 17), '050-523456789'),
-        ('Tal', 'talPassword', datetime(2020, 5, 17), '050-623456789'),
+        ('Ofek', 'ofekPassword', datetime(2020, 5, 17), '050-123456789', 'Manor'),
+        ('Danny', 'dannyPassword', datetime(2020, 5, 17), '050-223456789', 'Senderovych'),
+        ('Rimon', 'rimonPassword', datetime(2020, 5, 17), '050-323456789', 'Shushan'),
+        ('Rotem', 'rotemPassword', datetime(2020, 5, 17), '050-423456789', 'Eidlitz'),
+        ('Saar', 'saarPassword', datetime(2020, 5, 17), '050-523456789', 'Orus'),
+        ('Tal', 'talPassword', datetime(2020, 5, 17), '050-623456789', 'Koren'),
     ]
     with transaction.atomic():
-        for username, password, dob, phone_number in profile_data:
+        for username, password, dob, phone_number, last_name in profile_data:
             encrypted_pw = make_password(password)
             user = User(username=username, password=encrypted_pw)
+            user.first_name = username
+            user.last_name = last_name
             user.save()
             profile = Profile(user=user, date_of_birth=dob, phone_number=phone_number)
             profile.save()
