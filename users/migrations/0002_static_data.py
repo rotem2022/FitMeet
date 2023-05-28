779e81db6f57e77707b6d11dba5e90ef729f7692
Ofekmn <ofekm3000@gmail.com>
2023-05-28 11:28:03 +0300

    Fix user login
    Before it didn't allow to login as the password wasn't hashed.
    In addition changed the passwords so they would be easier to use.
    
    Signed-off-by: Ofekmn <ofekm3000@gmail.com>

--- users/migrations/0002_static_data.py
+++ users/migrations/0002_static_data.py
@@ -1,22 +1,24 @@
 from datetime import datetime
 
 from django.db import migrations, transaction
+from django.contrib.auth.hashers import make_password
 
 
 def create_initial_users(apps, schema_editor):
     Profile = apps.get_model('users', 'Profile')
     User = apps.get_model('auth', 'User')
     profile_data = [
-        ('Ofek', 'qaz[;.123', datetime(2020, 5, 17), '050-123456789'),
-        ('Danny', 'qaz[;.123', datetime(2020, 5, 17), '050-223456789'),
-        ('Rimon', 'qaz[;.123', datetime(2020, 5, 17), '050-323456789'),
-        ('Rotem', 'qaz[;.123', datetime(2020, 5, 17), '050-423456789'),
-        ('Saar', 'qaz[;.123', datetime(2020, 5, 17), '050-523456789'),
-        ('Tal', 'qaz[;.123', datetime(2020, 5, 17), '050-623456789'),
+        ('Ofek', 'ofekPassword', datetime(2020, 5, 17), '050-123456789'),
+        ('Danny', 'dannyPassword', datetime(2020, 5, 17), '050-223456789'),
+        ('Rimon', 'rimonPassword', datetime(2020, 5, 17), '050-323456789'),
+        ('Rotem', 'rotemPassword', datetime(2020, 5, 17), '050-423456789'),
+        ('Saar', 'saarPassword', datetime(2020, 5, 17), '050-523456789'),
+        ('Tal', 'talPassword', datetime(2020, 5, 17), '050-623456789'),
     ]
     with transaction.atomic():
         for username, password, dob, phone_number in profile_data:
-            user = User(username=username, password=password)
+            encrypted_pw = make_password(password)
+            user = User(username=username, password=encrypted_pw)
             user.save()
             profile = Profile(user=user, date_of_birth=dob, phone_number=phone_number)
             profile.save()
