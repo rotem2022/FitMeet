0c0099657570a73c999e5996f220333bff9c326f
Daniel Senderovych <danielsen@mta.ac.il>
2023-05-31 15:45:29 +0300

    Adding tests to make sure that the feed features are properly implemented
    Signed-off-by: Daniel Senderovych <danielsen@mta.ac.il>

--- event/tests/test_event_list_ui.py
+++ event/tests/test_event_list_ui.py
@@ -2,54 +2,58 @@ import pytest
 from ..models import Event
 
 
+@pytest.fixture
+def all_events():
+    return Event.manager.all()
+
+
+@pytest.fixture
+def logged_in_client(client, user1):
+    client.force_login(user1.user)
+    return client
+
+
 @pytest.mark.django_db
 class TestEventList:
-    def test_event_list(self, client, user1):
-        events = Event.manager.all()
-        client.force_login(user1.user)
-        response = client.get(f'/{user1.user.id}/event/event_list/')
+    def test_event_list(self, logged_in_client, user1, all_events):
+        response = logged_in_client.get(f'/{user1.user.id}/event/event_list/')
         assert response.status_code == 200
-        for event in events:
-            assert event.name in response.content.decode()
+        resp_content = response.content.decode()
+        for event in all_events:
+            assert event.name in resp_content
 
-    def test_event_list_filter_category(self, client, user1, category1):
-        events = Event.manager.all()
-        client.force_login(user1.user)
-        response = client.get(f'/{user1.user.id}/event/event_list/?Choose_Category={category1.name}')
+    def test_event_list_filter_category(self, logged_in_client, user1, all_events, category1):
+        response = logged_in_client.get(f'/{user1.user.id}/event/event_list/?Choose_Category={category1.name}')
         assert response.status_code == 200
-        for event in events.filter(category=category1):
-            assert event.name in response.content.decode()
-        for event in events.exclude(category=category1):
-            assert event.name not in response.content.decode()
+        resp_content = response.content.decode()
+        for event in all_events.filter(category=category1):
+            assert event.name in resp_content
+        for event in all_events.exclude(category=category1):
+            assert event.name not in resp_content
 
-    def test_event_list_filter_location(self, client, user1, location1):
-        events = Event.manager.all()
-        client.force_login(user1.user)
-        response = client.get(f'/{user1.user.id}/event/event_list/?Choose_Location={location1.name}')
+    def test_event_list_filter_location(self, logged_in_client, user1, all_events, location1):
+        response = logged_in_client.get(f'/{user1.user.id}/event/event_list/?Choose_Location={location1.name}')
         assert response.status_code == 200
-        for event in events.filter(location=location1):
-            assert event.name in response.content.decode()
-        for event in events.exclude(location=location1):
-            assert event.name not in response.content.decode()
+        resp_content = response.content.decode()
+        for event in all_events.filter(location=location1):
+            assert event.name in resp_content
+        for event in all_events.exclude(location=location1):
+            assert event.name not in resp_content
 
-    def test_event_list_order_by_time(self, client, user1):
-        events = Event.manager.all()
-        client.force_login(user1.user)
-        response = client.get(f'/{user1.user.id}/event/event_list/?Order_By=Time')
+    def test_event_list_order_by_time(self, logged_in_client, user1, all_events):
+        response = logged_in_client.get(f'/{user1.user.id}/event/event_list/?Order_By=Time')
         assert response.status_code == 200
-        events_ordered_by_time = sorted(events, key=lambda event: event.start_time)
-        content = response.content.decode()
-        event_names_in_html = [event.name for event in events_ordered_by_time if event.name in content]
+        events_ordered_by_time = sorted(all_events, key=lambda event: event.start_time)
+        resp_content = response.content.decode()
+        event_names_in_html = [event.name for event in events_ordered_by_time if event.name in resp_content]
         for i in range(len(events_ordered_by_time) - 1):
             assert event_names_in_html[i] == events_ordered_by_time[i].name
 
-    def test_event_list_order_by_participants(self, client, user1):
-        events = Event.manager.all()
-        client.force_login(user1.user)
-        response = client.get(f'/{user1.user.id}/event/event_list/?Order_By=Participants')
+    def test_event_list_order_by_participants(self, logged_in_client, user1, all_events):
+        response = logged_in_client.get(f'/{user1.user.id}/event/event_list/?Order_By=Participants')
         assert response.status_code == 200
-        events_ordered_by_participants = sorted(events, key=lambda event: event.max_participants)
-        content = response.content.decode()
-        event_names_in_html = [event.name for event in events_ordered_by_participants if event.name in content]
+        events_ordered_by_participants = sorted(all_events, key=lambda event: event.max_participants)
+        resp_content = response.content.decode()
+        event_names_in_html = [event.name for event in events_ordered_by_participants if event.name in resp_content]
         for i in range(len(events_ordered_by_participants)):
             assert event_names_in_html[i] == events_ordered_by_participants[i].name
